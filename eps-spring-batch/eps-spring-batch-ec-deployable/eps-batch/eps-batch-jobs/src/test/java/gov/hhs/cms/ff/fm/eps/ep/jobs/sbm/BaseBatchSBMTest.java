485e29112ea766872a7b4ccb4cec59fbf67310d3
JMichaelRad <j.radziewski@accenturefederal.com>
2016-12-08 07:20:10 -0500

    [Radziewski] Adding base unit test class for sbm batch jobs package.

---
+++ eps-spring-batch/eps-spring-batch-ec-deployable/eps-batch/eps-batch-jobs/src/test/java/gov/hhs/cms/ff/fm/eps/ep/jobs/sbm/BaseBatchSBMTest.java
@@ -0,0 +1,41 @@
+package gov.hhs.cms.ff.fm.eps.ep.jobs.sbm;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+
+import org.springframework.format.datetime.DateFormatter;
+
+import junit.framework.TestCase;
+
+public class BaseBatchSBMTest extends TestCase {
+
+	
+	protected final LocalDate DATE = LocalDate.now();
+	protected final LocalDateTime DATETIME = LocalDateTime.now();
+	protected final int YEAR = DATE.getYear();
+	protected final SimpleDateFormat sdf = new SimpleDateFormat("'D'yyMMdd'.T'HHmmssSSS");
+	protected final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("'D'yyMMdd'.T'HHmmssSSS");
+	protected final DateFormatter dateFormatter = new DateFormatter("yyyy-MM-dd HH:mm:ss.SSS");
+	
+	protected final LocalDate JAN_1 = LocalDate.of(YEAR, 1, 1);
+	protected final LocalDate JAN_15 = LocalDate.of(YEAR, 1, 15);
+	protected final LocalDate JAN_31 = LocalDate.of(YEAR, 1, 31);
+	protected final LocalDate FEB_1 = LocalDate.of(YEAR, 2, 1);
+	protected final LocalDate FEB_MAX = DATE.with(TemporalAdjusters.lastDayOfMonth());
+	protected final LocalDate MAR_1 = LocalDate.of(YEAR, 3, 1);
+	protected final LocalDate MAR_31 = LocalDate.of(YEAR, 3, 31);
+	protected final LocalDate APR_1 = LocalDate.of(YEAR, 4, 1);
+	
+	protected final LocalDate NOV_1 = LocalDate.of(YEAR, 11, 1);
+	protected final LocalDate DEC_1 = LocalDate.of(YEAR, 12, 1);
+	protected final LocalDate DEC_31 = LocalDate.of(YEAR, 12, 31);
+	
+	protected final LocalDateTime JAN_1_1am = LocalDateTime.of(YEAR, 1, 1, 1, 0, 0, 111111000);
+	protected final LocalDateTime FEB_1_2am = LocalDateTime.of(YEAR, 2, 1, 2, 0, 0, 222222000);
+	protected final LocalDateTime MAR_1_3am = LocalDateTime.of(YEAR, 3, 1, 3, 0, 0, 333333000);
+	protected final LocalDateTime APR_1_4am = LocalDateTime.of(YEAR, 4, 1, 4, 0, 0, 444444000);
+	protected final LocalDateTime JUN_1_1am = LocalDateTime.of(YEAR, 6, 1, 1, 0, 0, 666666000);
+}
